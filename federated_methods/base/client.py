69e2cecc4b08f0c33c55b901bbcf15af0c9f06c0
jetmind2 <jetmind2>
2026-07-08 12:33:25 +0300

    exp

--- federated_methods/base/client.py
+++ federated_methods/base/client.py
@@ -36,7 +36,7 @@ class Client:
         self.server_model_state = None
 
         # УБРАТЬ ЕСЛИ КОЛИЧЕСТВО ШАГОВ ЛОКАЛЬНЫХ НЕ 1
-        # self.old_model = None
+        self.old_model = None
 
         self.metrics_threshold = cfg.training_params.metrics_threshold
         self.print_metrics = cfg.federated_params.print_client_metrics
@@ -165,7 +165,7 @@ class Client:
 
         return val_loss / len(self.valid_loader), client_metrics
 
-
+    '''
     def get_grad(self):
         self.model.eval()
         for key, _ in self.model.state_dict().items():
@@ -180,7 +180,7 @@ class Client:
             self.grad[key] = self.model.state_dict()[key].to(
                 "cpu"
             ) - self.old_model[key].to("cpu")
-    '''
+
     def train(self):
         # Save the server model state to get_grad
         self.server_model_state = copy.deepcopy(self.model).state_dict()
